94b3ac374509354d7d43d70c8dc47650b9b9bc13
Larry Hsiao <larryhsiao@silverhetch.com>
2017-12-15 00:49:19 +0800

    initial list layout with fragment

--- app/src/main/java/com/silverhetch/athena/MainActivity.java
+++ app/src/main/java/com/silverhetch/athena/MainActivity.java
@@ -1,13 +1,18 @@
 package com.silverhetch.athena;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_vocabulary);
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.vocabularyList_recyclerView, VocabularyListFragment.newInstance());
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/silverhetch/athena/VocabularyListFragment.java
@@ -0,0 +1,40 @@
+package com.silverhetch.athena;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.silverhetch.athena.databinding.FragmentVocabularyBinding;
+
+/**
+ * Created by mikes on 12/15/2017.
+ */
+
+public class VocabularyListFragment extends Fragment {
+    private FragmentVocabularyBinding binding;
+
+    public static Fragment newInstance() {
+        return new VocabularyListFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_vocabulary, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
